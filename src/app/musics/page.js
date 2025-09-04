fbea6afb23fb43f5603b26855138768919125e62
Pedro Kuczera <pedrohkd001@gmail.com>
2025-09-04 17:15:38 -0300

    Listando categorias

--- src/app/musics/page.js
+++ src/app/musics/page.js
@@ -1,10 +1,12 @@
 import HighlightVideo from '@/components/HighlightVideo'
 import style from './page.module.css'
+import VideoTags from '@/components/VideoTags'
 
 export default function MusicsPage() {
     return (
         <>
             <HighlightVideo tag='Músicas' />
+            <VideoTags tag='Músicas' />
         </>
     )
 }
