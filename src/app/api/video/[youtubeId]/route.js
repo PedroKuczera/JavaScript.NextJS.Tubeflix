b57358cbc744cd8763e23d0f04b5552b88ca9634
Pedro Kuczera <pedrohkd001@gmail.com>
2025-09-15 08:50:40 -0300

    Lista de videos

--- src/app/api/video/[youtubeId]/route.js
+++ src/app/api/video/[youtubeId]/route.js
@@ -4,7 +4,7 @@ import { getVideos } from '@/helpers/helpers-server';
 
 export async function GET(request, { params }) {
     const { youtubeId } = await params
-    const videos = await getVideos()
+    const videos = await getVideos() 
     const videosList = objectToArray(videos)
     const video = videosList.find(video => video.key === youtubeId) ?? null
     return NextResponse.json(video, { status: video ? 200 : 404 })
